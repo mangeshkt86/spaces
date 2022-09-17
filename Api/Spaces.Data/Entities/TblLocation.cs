e4a3169047a97a46888915754b687f0cdce792d4
mangeshkt86 <mangeshkt.it@gmail.com>
2022-09-17 20:11:32 +0530

    Added some constraints into db tables

--- Api/Spaces.Data/Entities/TblLocation.cs
+++ Api/Spaces.Data/Entities/TblLocation.cs
@@ -5,8 +5,15 @@ namespace Spaces.Data.Entities
 {
     public partial class TblLocation
     {
+        public TblLocation()
+        {
+            TblFloors = new HashSet<TblFloor>();
+        }
+
         public long Id { get; set; }
-        public string? Name { get; set; }
-        public string? Status { get; set; }
+        public string Name { get; set; } = null!;
+        public string Status { get; set; } = null!;
+
+        public virtual ICollection<TblFloor> TblFloors { get; set; }
     }
 }
--- Api/Spaces.Data/Entities/TblZone.cs
+++ Api/Spaces.Data/Entities/TblZone.cs
@@ -5,9 +5,17 @@ namespace Spaces.Data.Entities
 {
     public partial class TblZone
     {
+        public TblZone()
+        {
+            TblDesks = new HashSet<TblDesk>();
+        }
+
         public long Id { get; set; }
         public long FloorId { get; set; }
-        public string? Name { get; set; }
-        public string? Status { get; set; }
+        public string Name { get; set; } = null!;
+        public string Status { get; set; } = null!;
+
+        public virtual TblFloor Floor { get; set; } = null!;
+        public virtual ICollection<TblDesk> TblDesks { get; set; }
     }
 }
