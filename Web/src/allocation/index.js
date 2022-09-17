5e01b0857e61beabf175c97d81f143bc90652005
Pradeep Motar <pradeep.motar@gmail.com>
2022-09-17 23:09:14 +0530

    basic component and menu added

---
+++ Web/src/allocation/index.js
@@ -0,0 +1,9 @@
+import React, { useState, Fragment } from 'react';
+
+export default function Allocation() {
+    return (
+        <Fragment>
+            <h1>Allocation</h1>
+        </Fragment>
+    )
+}
