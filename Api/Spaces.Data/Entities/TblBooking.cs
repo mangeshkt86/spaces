5dcc4387f8e94edf4e899de3934f109d991af513
pawankand <kand.pawan22@gmail.com>
2022-09-17 23:47:35 +0530

    Added API and Odata for allocation, booking and departments

---
+++ Api/Spaces.Data/Entities/TblBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spaces.Data.Entities
+{
+    public partial class TblBooking
+    {
+        public long Id { get; set; }
+        public string? UserId { get; set; }
+        public string? DeskNumber { get; set; }
+        public string? IsBooked { get; set; }
+        public string? BookedOn { get; set; }
+        public string? BookedFor { get; set; }
+    }
+}
