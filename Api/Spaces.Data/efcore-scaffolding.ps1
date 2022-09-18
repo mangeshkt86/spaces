f891e5340fd830915c0974df42c3ae01f853bc2f
pawankand <kand.pawan22@gmail.com>
2022-09-18 13:51:49 +0530

    Added CRUD operations for allocation and bookings

--- Api/Spaces.Data/efcore-scaffolding.ps1
+++ Api/Spaces.Data/efcore-scaffolding.ps1
@@ -1,2 +1,2 @@
 write-host "Generating entities and db context from sqlite db.."
-dotnet ef dbcontext scaffold "Data Source=C:\Users\899807\learning\SpaceAllocationTool\Database\space_allocation.db;" Microsoft.EntityFrameworkCore.Sqlite -o Entities --context-dir Infrastructure -c SpacesDbContext --context-namespace Spaces.Data --force
+dotnet ef dbcontext scaffold "Data Source=..\..\Database\space_allocation.db;" Microsoft.EntityFrameworkCore.Sqlite -o Entities --context-dir Infrastructure -c SpacesDbContext --context-namespace Spaces.Data --force
